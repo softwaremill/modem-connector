a38ed56e310f9edba2c5e5a4ffff013b9ff6bba9
Tomasz Luczak <tomasz.luczak@softwaremill.com>
2016-12-02 09:54:07 +0100

    Frame sender types fix

--- src/main/scala/com/softwaremill/agwpe/Sender.scala
+++ src/main/scala/com/softwaremill/agwpe/Sender.scala
@@ -15,10 +15,10 @@ object Sender {
   private def frameBytes(port: Int, dataKind: Char, pid: Int, callFrom: String, callTo: String, data: Array[Byte]): List[Int] = {
     def callBytes(string: String): List[Int] = (string.toList.map(_.toInt) ++ List.fill(IdSize - string.length)(0)).take(IdSize)
     def dataBytes(dataOpt: Option[Array[Byte]]): List[Int] = dataOpt match {
-      case None => List.fill(Byte)(0)
-      case Some(someData) => List.tabulate(HalfByte)(pos => someData.length >>> (Byte*pos)) ++ List.fill(HalfByte)(0) ++ someData
+      case None => List.fill[Int](Byte)(0)
+      case Some(someData) => List.tabulate[Int](HalfByte)(pos => someData.length >>> (Byte*pos)) ++ List.fill[Int](HalfByte)(0) ++ someData.map(_.toInt)
     }
-    val headerBytes = port :: 0 :: 0 :: 0 :: dataKind :: 0 :: pid :: 0 :: Nil
+    val headerBytes = port :: 0 :: 0 :: 0 :: dataKind.toInt :: 0 :: pid :: 0 :: Nil
     headerBytes ++ callBytes(callFrom) ++ callBytes(callTo) ++ dataBytes(Option(data))
   }
 
