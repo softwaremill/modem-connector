24ea109c3d18454531a68c17bac7683ef8fce68b
softberries <krzysztof.grajek@googlemail.com>
2016-12-05 09:47:05 +0100

    #49 small refactor

--- src/test/scala/com/softwaremill/agwpe/AGWPEFrameSpec.scala
+++ src/test/scala/com/softwaremill/agwpe/AGWPEFrameSpec.scala
@@ -4,11 +4,11 @@ import java.io.{DataInputStream, File, FileInputStream}
 
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
-class AGWPEFrameSpec extends FlatSpec with Matchers with BeforeAndAfter{
+class AGWPEFrameSpec extends FlatSpec with Matchers with BeforeAndAfter {
 
   "An AGWPEFrame" should "be created from binary data received from SoundModem" in {
-    val dis:DataInputStream = new DataInputStream(new FileInputStream(file("/getInfoQuery.bin")))
-    val frame:AGWPEFrame = AGWPEFrame(dis)
+    val dis: DataInputStream = dataStream("/getInfoQuery.bin")
+    val frame: AGWPEFrame = AGWPEFrame(dis)
     frame.dataKind.toChar shouldEqual 'R'
   }
 
@@ -18,12 +18,15 @@ class AGWPEFrameSpec extends FlatSpec with Matchers with BeforeAndAfter{
       541982 B   2718 SWPC  @WW     CX2SA  130424 Report of Solar-Geophysical Activi
       541983 B   1571 EQUAKE@WW     CX2SA  130424 NEW IRELAND REGION, PAPUA NEW GUIN
      */
-    val dis:DataInputStream = new DataInputStream(new FileInputStream(file("/dataFrame.bin")))
-    val frame:AGWPEFrame = AGWPEFrame(dis)
+    val dis: DataInputStream = dataStream("/dataFrame.bin")
+    val frame: AGWPEFrame = AGWPEFrame(dis)
     val data = new String(frame.data.get)
-    data should include ("541982")
-    data should include ("PAPUA NEW GUIN")
+    data should include("541982")
+    data should include("PAPUA NEW GUIN")
   }
 
-  def file(path: String): File = new File(this.getClass.getResource(path).getPath)
+  def dataStream(filePath: String): DataInputStream = {
+    val file: File = new File(this.getClass.getResource(filePath).getPath)
+    new DataInputStream(new FileInputStream(file))
+  }
 }
