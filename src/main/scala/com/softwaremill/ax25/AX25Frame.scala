42bc2724e3d7ff80973a53a9785d847c5539d11a
Tomasz Luczak <tomasz.luczak@softwaremill.com>
2016-12-07 15:58:49 +0100

    #50 AX25 parser refactor

--- src/main/scala/com/softwaremill/ax25/AX25Frame.scala
+++ src/main/scala/com/softwaremill/ax25/AX25Frame.scala
@@ -1,7 +1,5 @@
 package com.softwaremill.ax25
 
-import scala.collection.mutable.ListBuffer
-
 class AX25Frame(val sender: AX25Callsign, val dest: AX25Callsign,
                 val digipeaters: Array[AX25Callsign],
                 val ctl: Byte,
@@ -16,34 +14,42 @@ object AX25Frame {
   val FrameTypeU: Int = 3
   val UTypeUi = 0x00
 
+  val CallsignSize: Int = 7
+  val Offset: Int = 1
+
   def apply(data: Array[Byte]): AX25Frame = {
-    var offset: Int = 1
     def callsign(pos: Int) = AX25Callsign(data.slice(pos, pos+7))
 
-    val dest: AX25Callsign = callsign(offset)
-    val sender: AX25Callsign = callsign(offset+7)
+    val dest: AX25Callsign = callsign(Offset)
+    val senderPos: Int = Offset + CallsignSize
+    val sender: AX25Callsign = callsign(senderPos)
 
-    val digipeaters: ListBuffer[AX25Callsign] = ListBuffer.empty
-    offset = 15
-    while ((data(offset-1) & 0x01) == 0) {
-      digipeaters += callsign(offset)
-      offset += 7
+    def retriveDigipeaters(offset: Int, digipeaters: List[AX25Callsign]): List[AX25Callsign] = {
+      if ((data(offset-1) & 0x01) == 0) {
+          retriveDigipeaters(offset + CallsignSize, digipeaters) ++ digipeaters
+      } else {
+        digipeaters
+      }
     }
+    val digipeatersPos: Int = senderPos + CallsignSize
+    val digipeaters: List[AX25Callsign] = retriveDigipeaters(digipeatersPos, List.empty)
 
-    val ctl: Byte = data(offset)
-    offset = offset + 1
+    val ctlPos: Int = digipeatersPos + digipeaters.size*CallsignSize
+    val ctl: Byte = data(ctlPos)
 
     def pid(pos: Int): Option[Byte] = {
       if ((ctl & 0x01) == 0 || (ctl & (MaskUType | MaskFrameType)) == (UTypeUi | FrameTypeU)) {
-        offset += 1
         Some(data(pos))
       } else {
         None
       }
     }
-    val pidOpt: Option[Byte] = pid(offset)
+    val pidOptPos: Int = ctlPos + 1
+    val pidOpt: Option[Byte] = pid(Offset + 2 * CallsignSize + digipeaters.size * Offset + 1)
 
-    new AX25Frame(sender, dest, digipeaters.toArray, ctl, pidOpt, data.slice(offset, data.length))
+    implicit def bool2int(b:Boolean) = if (b) 1 else 0
+    val dataPos: Int = pidOptPos + pidOpt.nonEmpty
+    new AX25Frame(sender, dest, digipeaters.toArray, ctl, pidOpt, data.slice(dataPos, data.length))
   }
 
 }
