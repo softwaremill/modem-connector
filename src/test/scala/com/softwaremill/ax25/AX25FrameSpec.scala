0a24643c52b43e753157c8844776025ff1002a28
Tomasz Luczak <tomasz.luczak@softwaremill.com>
2016-12-07 15:36:05 +0100

    #50 AX25 sender and destination test cases

--- src/test/scala/com/softwaremill/ax25/AX25FrameSpec.scala
+++ src/test/scala/com/softwaremill/ax25/AX25FrameSpec.scala
@@ -9,14 +9,24 @@ import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 class AX25FrameSpec extends FlatSpec with Matchers with BeforeAndAfter {
 
   "An AX25Frame" should "contain decoded data" in {
-    val ax24frame: AX25Frame = AX25Frame(AGWPEFrame(dataStream("/andrej.bin")).data.get)
+    val ax24frame: AX25Frame = ax25frameFromFile("/andrej.bin")
     val decodedFrameBody = new String(ax24frame.body)
     decodedFrameBody should equal("=4603.63N/01431.26E-Op. Andrej")
   }
 
-  def dataStream(filePath: String): DataInputStream = {
+  "An AX25Frame" should "contain sender callsign" in {
+    val ax24frame: AX25Frame = ax25frameFromFile("/andrej.bin")
+    ax24frame.sender.callsign should equal("S57LN")
+  }
+
+  "An AX25Frame" should "contain destination callsign" in {
+    val ax24frame: AX25Frame = ax25frameFromFile("/andrej.bin")
+    ax24frame.dest.callsign should equal("APRS")
+  }
+
+  def ax25frameFromFile(filePath: String): AX25Frame = {
     val file: File = new File(this.getClass.getResource(filePath).getPath)
-    new DataInputStream(new FileInputStream(file))
+    AX25Frame(AGWPEFrame(new DataInputStream(new FileInputStream(file))).data.get)
   }
 
 }
